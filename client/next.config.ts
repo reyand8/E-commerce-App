e400dd02293e3500d01eb3d85ca7447134164138
Andrey <andreeey.gg@gmail.com>
2024-11-29 16:09:30 +0100

    add client config

---
+++ client/next.config.ts
@@ -0,0 +1,32 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        esmExternals: false,
+    },
+    env: {
+        REACT_APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
+    },
+    async headers() {
+        return [
+            {
+                source: "/_next/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
+                ],
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/uploads/:path*',
+                destination: 'http://localhost:5000/uploads/:path*',
+            },
+        ];
+    },
+};
+
+export default nextConfig;
---
+++ client/tailwind.config.ts
@@ -0,0 +1,72 @@
+import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
+
+const constants = {
+  green: '#018c63',
+  'dark-green': '#01573f',
+  'light-green': '#dae7e3',
+  black: '#1e1d1d',
+  white: '#ffffff',
+  'light-gray': '#e5e4e0',
+  red: '#de3637',
+  beige: '#9d9589',
+  'dark-beige': '#605b57',
+};
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      transparent: colors.transparent,
+      ...constants,
+    },
+    extend: {
+      fontSize: {
+        xs: '0.82rem',
+        sm: '0.98rem',
+        base: '1.15rem',
+        lg: '1.22rem',
+        xl: '1.36rem',
+        '1.5xl': '1.5rem',
+        '2xl': '1.725rem',
+        '3xl': '2.155rem',
+        '4xl': '2.58rem',
+        '5xl': '3.45rem',
+        '6xl': '4.3rem',
+        '7xl': '5.17rem',
+        '8xl': '6.9rem',
+        '9xl': '9.2rem',
+      },
+      keyframes: {
+        animationOpacity: {
+          from: { opacity: '0.2' },
+          to: { opacity: '1' },
+        },
+        scaleIn: {
+          '0%': {
+            opacity: '0',
+            transform: 'scale(0.9)',
+          },
+          '50%': {
+            opacity: '0.3',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'scale(1)',
+          },
+        },
+      },
+      animation: {
+        opacity: 'animationOpacity .5s ease-in-out',
+        scaleIn: 'scaleIn .35s ease-in-out',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
