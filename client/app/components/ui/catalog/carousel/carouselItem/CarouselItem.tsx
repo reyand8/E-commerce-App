460b94d71c3fe0d5bdc71a6c5143472f749df5a2
Andrey <andreeey.gg@gmail.com>
2024-12-05 18:59:57 +0100

    add types and add cat

--- client/app/components/ui/catalog/carousel/carouselItem/CarouselItem.tsx
+++ client/app/components/ui/catalog/carousel/carouselItem/CarouselItem.tsx
@@ -8,57 +8,50 @@ import { SizeVariations } from '@/app/components/ui/catalog/carousel/carouselIte
 import { CarouselNavigation } from '@/app/components/ui/catalog/carousel/carouselItem/carouselNavigation/CarouselNavigation';
 import { useCarousel } from '@/app/components/ui/catalog/carousel/carouselItem/useCarousel';
 import { ICarouselItem } from '@/types/carousel.interface';
-import {TypeSize} from "@/types/sliceCart.interface";
-import {useActions} from "@/app/hooks/useActions";
+import {TypeSize} from '@/types/sliceCart.interface';
+import {useActions} from '@/app/hooks/useActions';
 import styles from '../Carousel.module.scss';
 
 
-export const CarouselItem: FC<ICarouselItem> = ({product, qty, key, index}) => {
+export const CarouselItem: FC<ICarouselItem> = ({ product, qty, index, key, cat }) => {
 	const [selectedSize, setSelectedSize] = useState<TypeSize>('SHORT');
-	const { selectedItemIndex } = useCarousel();
+	const { selectedItemIndex } = useCarousel(cat);
 	const { selectSlide } = useActions();
-	const isActive: boolean = index === selectedItemIndex;
+	const isActive = index === selectedItemIndex;
 
 	return (
-		<motion.div className={cn(styles.item, {
-			[styles.active]: isActive
-		})}
-	    initial={{ scale: 1 }}
+		<motion.div
+			className={cn(styles.item, { [styles.active]: isActive })}
+			initial={{ scale: 1 }}
 			animate={isActive ? { scale: 1.19 } : {}}
 			transition={{ duration: 0.6, type: 'tween' }}
-			aria-label='Select-item'
-			role='button'
-		>
+			aria-label="Select-item"
+			role="button"
+			key={key}>
 			<div>
 				<CarouselNavigation
-					onSelectedSlide={() => selectSlide(index)}
+					onSelectedSlide={() => selectSlide({ carouselId: cat, index })}
 					product={product}
 					isActive={isActive}
-					qty={qty}/>
-				<div className={styles.heading} onClick={() => selectSlide(index)}>
-					<span>
-						{product.name}
-					</span>
+					qty={qty}
+					cat={cat}
+				/>
+				<div className={styles.heading} onClick={() => selectSlide({ carouselId: cat, index })}>
+					<span>{product.name}</span>
 				</div>
-
-				{isActive ?
+				{isActive ? (
 					<>
-						<SizeVariations
-							selectedSize={selectedSize}
-							setSelectedSize={setSelectedSize}
-						/>
-						<AddToCartButton
-							product={product}
-							selectedSize={selectedSize}
-						/>
-						<Link href={`/product/${product.slug}`} className={styles.link}>
-							More details
-						</Link>
-					</> :
-					<div className={styles.information}>
-						{product.description}
-					</div>
-				}
+						{
+							cat === 'Drinks' ? (
+								<SizeVariations selectedSize={selectedSize} setSelectedSize={setSelectedSize} />
+							) : <></>
+						}
+						<AddToCartButton product={product} selectedSize={selectedSize} />
+						<Link href={`/product/${product.slug}`} className={styles.link}>More details</Link>
+					</>
+				) : (
+					<div className={styles.information}>{product.description}</div>
+				)}
 			</div>
 		</motion.div>
 	);
