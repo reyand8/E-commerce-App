ab4f6f61c4df07473edccb5981daa253b297a1e5
Andrey <andreeey.gg@gmail.com>
2024-12-05 18:54:44 +0100

    add cat and code refactoring

--- client/app/components/ui/catalog/carousel/Carousel.tsx
+++ client/app/components/ui/catalog/carousel/Carousel.tsx
@@ -2,10 +2,10 @@ import { FC } from 'react';
 
 import styles from './Carousel.module.scss';
 import { IProduct } from '@/types/product.interface';
-import {CarouselItem} from "@/app/components/ui/catalog/carousel/carouselItem/CarouselItem";
+import { CarouselItem } from '@/app/components/ui/catalog/carousel/carouselItem/CarouselItem';
 
 
-export const Carousel: FC<{ products: IProduct[] }> = ({ products }) => {
+export const Carousel: FC<{ products: IProduct[]; cat: string }> = ({ products, cat }) => {
 	return (
 		<section className={styles.carousel}>
 			{ products.map((product: IProduct, index: number) =>
@@ -14,6 +14,7 @@ export const Carousel: FC<{ products: IProduct[] }> = ({ products }) => {
 					qty={products.length}
 					key={index}
 					index={index}
+					cat={cat}
 				/>
 			)}
 		</section>
