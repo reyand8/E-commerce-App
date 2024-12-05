79e0b2382f229f6de6bbb8cdc8991b36da4fb524
Andrey <andreeey.gg@gmail.com>
2024-12-05 19:35:35 +0100

    add Layout

---
+++ client/app/components/layout/Layout.tsx
@@ -0,0 +1,25 @@
+import { FC, PropsWithChildren } from 'react';
+
+import Meta from './meta/Meta';
+import { ISeo } from './meta/meta.interface';
+import styles from './Layout.module.scss';
+import { Header } from './header/Header';
+
+
+interface ILayout extends ISeo {}
+
+const Layout: FC<PropsWithChildren<ILayout>> = ({ children, ...rest }) => {
+    return (
+      <>
+        <Meta {...rest} />
+        <div className={styles.layout}>
+          <main>
+            <Header />
+            <section className={styles.content}>{ children }</section>
+          </main>
+        </div>
+      </>
+    );
+};
+
+export default Layout;
