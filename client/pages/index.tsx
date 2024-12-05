817e80733e9d48a1893bdc4dbab5ac7ef6f652a9
Andrey <andreeey.gg@gmail.com>
2024-12-05 18:43:10 +0100

    remove getStaticProps

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,21 +1,10 @@
-import { GetStaticProps, NextPage } from 'next';
+import { NextPage } from 'next';
 
-import {ProductService} from '@/services/productService';
-import {IProduct, IProductsPage} from '@/types/product.interface';
 import Home from '../app/components/screens/home/Home';
 
 
-const HomePage: NextPage<IProductsPage> = ({ products }) => {
-    return <Home products={products}/>;
-};
-
-export const getStaticProps: GetStaticProps<IProductsPage> = async () => {
-    const products: IProduct[] = await ProductService.getProducts();
-    return {
-        props: {
-            products,
-        },
-    };
+const HomePage: NextPage = () => {
+    return <Home />;
 };
 
 export default HomePage;
