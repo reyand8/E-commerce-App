077875d10d1942c773c7b7897a8643be5ab3a559
Andrey <andreeey.gg@gmail.com>
2024-12-05 18:46:37 +0100

    add useActions and useCarousel

--- client/app/components/ui/catalog/carousel/carouselItem/useCarousel.ts
+++ client/app/components/ui/catalog/carousel/carouselItem/useCarousel.ts
@@ -1,5 +1,7 @@
 import { useTypedSelector } from '@/app/hooks/useTypedSelector';
 import {TypeRootState} from "@/store/store";
 
-export const useCarousel = () =>
-    useTypedSelector((state: TypeRootState) => state.carousel);
+export const useCarousel = (cat: string) => {
+    const selectedItemIndex = useTypedSelector((state: TypeRootState) => state.carousel[cat]);
+    return { selectedItemIndex };
+};
