706496ef50a076922b412feb2776098f40a27990
Andrey <andreeey.gg@gmail.com>
2024-12-16 15:05:22 +0100

    add footer and contact page

---
+++ client/pages/contacts/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next';
+
+import Contacts from '@/components/screens/contacts/Contacts';
+
+
+const ContactsPage: NextPage = () => {
+    return <Contacts />;
+};
+
+export default ContactsPage;
